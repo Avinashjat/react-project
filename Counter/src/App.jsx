6227b71f68f2705375df01571fecea593468b46e
Avinash Jat <dev.avinashjat@gmail.com>
2024-02-25 00:16:11 +0530

    make a counter button

---
+++ Counter/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+ 
+
+let [counter , setCounter] = useState(0)
+ 
+
+let addvalue = ()=>{
+  setCounter(counter+1)
+}
+
+
+let removevalue = ()=>{
+ 
+
+  if(counter==0){
+    setCounter(0)
+  }else{
+    setCounter(counter-1)
+  }
+}
+
+  return (
+    <>
+      
+<h1> counter value {counter}</h1>
+
+<button onClick={addvalue}> Add value   </button>
+
+<br /> 
+<br />
+<button onClick={removevalue}> Remove  value  </button>
+
+    </>
+  )
+}
+
+export default App
